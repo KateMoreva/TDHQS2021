fe0becbcc1accc4e0384fb2997e8cdecfc95d1e5
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2021-11-25 13:05:39 +0300

    TDHQS2021-24-local-db-connection
    + More for db
    + architecture
    + loginn activity

---
+++ app/src/main/java/map/together/dto/db/CategoryDto.kt
@@ -0,0 +1,17 @@
+package map.together.dto.db
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+data class CategoryDto(
+        @SerializedName("id")
+        @Expose
+        val id: Long,
+        @SerializedName("name")
+        @Expose
+        var name: String,
+        @SerializedName("owner_id")
+        @Expose
+        var ownerId: Long,
+)
