03cd2bba4ab4507898a204846359426bcd2cc574
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2021-12-14 23:10:19 +0300

    Api

--- app/src/main/java/map/together/api/ApiInterface.kt
+++ app/src/main/java/map/together/api/ApiInterface.kt
@@ -1,8 +1,13 @@
 package map.together.api
 
+import com.mikepenz.fastadapter.GenericItem
 import io.reactivex.Single
 import map.together.dto.UserDto
 import map.together.dto.UserSignUpDto
+import map.together.dto.db.CategoryDto
+import map.together.dto.db.LayerDto
+import map.together.dto.db.MapDto
+import map.together.dto.db.PlaceDto
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -19,16 +24,90 @@ import retrofit2.http.Path
 interface ApiInterface {
 
     @Headers("Content-Type: application/json")
-    @POST("/register")
+    @POST("/auth/registration")
     fun createUserRequest(@Body userSignUpDto: UserSignUpDto): Single<Response<UserDto>>
 
-    @GET("/users/login")
-    fun loginRequest(@Header("Authorization") token: String): Single<Response<UserDto>>
+    @GET("/auth/login")
+    fun loginRequest(): Single<Response<UserDto>>
 
-    @PATCH("/users/{id}")
+    @POST("/auth/profile/change")
     fun changeUserData(
-        @Path("id") id: Long,
-        @Header("Authorization") token: String,
-        @Body nameDto: Any
+        userName: String,
+        passwordHash: String,
+        oldPassword: String,
+        picture: Any
     ): Single<Response<UserDto>>
+
+    @GET("/owner/places")
+    fun getMyPlaces(search: String?): Single<Response<List<PlaceDto>>>
+
+    @GET("/owner/categories")
+    fun getMyCategories(search: String?): Single<Response<List<CategoryDto>>>
+
+    @GET("/owner/layers")
+    fun getMyLayers(search: String?): Single<Response<List<LayerDto>>>
+
+    @GET("/owner/maps")
+    fun getMyMaps(search: String?): Single<Response<List<MapDto>>>
+
+    @POST("/owner/maps/create")
+    fun createMap(mapName: String): Single<Response<MapDto>>
+
+    @POST("/owner/categories/create")
+    fun createCategory(categoryName: String): Single<Response<CategoryDto>>
+
+    @POST("/owner/categories/change")
+    fun changeCategory(categoryId: Long, newName: String): Single<Response<CategoryDto>>
+
+    @POST("/shared/users/change_role")
+    fun changeUserRole(mapId: Long, email: String, role: Long): Single<Response<UserDto>>
+
+    @GET("/shared/users/list")
+    fun getUsersByMap(mapId: Long, search: String?): Single<Response<List<UserDto>>>
+
+    @POST("/shared/places/remove")
+    fun removePlaceByLayerAndMap(
+        mapId: Long,
+        layerId: Long,
+        placeId: Long
+    ): Single<Response<PlaceDto>>
+
+    @POST("/shared/places/change")
+    fun changePlace(
+        mapId: Long,
+        layerId: Long,
+        placeId: Long,
+        newName: String
+    ): Single<Response<PlaceDto>>
+
+    @POST("/shared/places/create")
+    fun createPlace(
+        placeName: String,
+        mapId: Long,
+        layerId: Long,
+        latitude: String,
+        longitude: String,
+        categoryId: Long
+    ): Single<Response<PlaceDto>>
+
+    @POST("/shared/maps/remove")
+    fun removeMap(mapId: Long): Single<Response<MapDto>>
+
+    @POST("/shared/maps/change")
+    fun changeMap(mapId: Long, newName: String): Single<Response<MapDto>>
+
+    @POST("/shared/layers/change")
+    fun changeLayer(mapId: Long, layerId: Long, newName: String): Single<Response<LayerDto>>
+
+    @GET("/shared/layers/list")
+    fun getLayersByMap(mapId: Long, search: String?): Single<Response<List<LayerDto>>>
+
+    @POST("/shared/layers/remove")
+    fun removeLayerByMap(mapId: Long, layerId: Long): Single<Response<LayerDto>>
+
+    @POST("/shared/layers/create")
+    fun createLayer(LayerName: String, mapId: Long): Single<Response<LayerDto>>
+
+    @POST("/shared/layers/demonstrate")
+    fun demonstrateLayers(mapId: Long, layersIds: List<Long>): Single<Response<List<Long>>>
 }
